495599619cf6ba8a3d71a3fe729bea58ae3a4f42
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-19 16:54:14 +0200

    chore: remove jest

--- docs/CLI.md
+++
@@ -1,5 +0,0 @@
-```
-git clone https://github.com/AlbertoBasalo/quark your-project
-cd your-project
-npm install
-```
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-import { NAME } from "./dependency";
-import { sayHello, sum } from "./functions";
-
-const myName = NAME;
-sayHello(myName);
-console.log(sum(1, 2));
