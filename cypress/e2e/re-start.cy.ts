e48d4d85511c4d9d6926246889f6489a59652ecc
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-19 19:20:21 +0200

    test: home page

--- cypress/e2e/re-start.cy.ts
+++ cypress/e2e/re-start.cy.ts
@@ -1,29 +1,26 @@
 describe("The Activity Bookings home page", () => {
-  it("should be visitable", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:4200/");
   });
+  it("should be visitable", () => {
+    cy.get("html").should("exist");
+  });
   it("should have an h1", () => {
-    cy.visit("http://localhost:4200/");
     cy.get("h1").should("exist");
   });
   it("should have an h1 with 'Activity Bookings' text", () => {
-    cy.visit("http://localhost:4200/");
-    cy.get("h1").contains("Activity Bookings");
+    cy.get("h1").should("have.text", "Activity Bookings");
   });
   it("should contains '© Alberto Basalo' in the footer", () => {
-    cy.visit("http://localhost:4200/");
     cy.get("footer").contains("© Alberto Basalo");
   });
   it("should have a link to albertobasalo.dev", () => {
-    cy.visit("http://localhost:4200/");
     cy.get('[href="https://albertobasalo.dev"]').should("exist");
   });
   it("should have an underline element with 'Course sample' content", () => {
-    cy.visit("http://localhost:4200/");
     cy.get("u").contains("Course sample");
   });
   it("should have a link with css class 'secondary'", () => {
-    cy.visit("http://localhost:4200/");
     cy.get("a.secondary").should("exist");
   });
 });
