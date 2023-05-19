230223f4489949ad6e1c20f3e35ea01ae4f73de6
Alberto Basalo <albertobasalo@hotmail.com>
2023-05-19 17:40:34 +0200

    chore: green

--- cypress/e2e/start.cy.ts
+++ cypress/e2e/start.cy.ts
@@ -1,8 +1,13 @@
 describe("The Activity Bookings home page", () => {
-  it("should exist", () => {
+  it("should be visitable", () => {
     cy.visit("http://localhost:4200/");
   });
   it("should have a title", () => {
-    cy.get("h1").should("contain", "Activity Bookings");
+    cy.visit("http://localhost:4200/");
+    cy.get("h1").should("exist");
+  });
+  it("should have a title with 'Activity Bookings'", () => {
+    cy.visit("http://localhost:4200/");
+    cy.get("h1").should("have.text", "Activity Bookings");
   });
 });
