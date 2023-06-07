ae259963297f0c65c49f047e471265e753b41bf2
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-07 19:10:22 +0200

    docs: test descriptors

--- cypress/e2e/3-sign_up-form.cy.ts
+++ cypress/e2e/3-sign_up-form.cy.ts
@@ -1,3 +1,20 @@
+/**
+ * The sign-up form
+ *   when the users fills the form correctly
+ *     should have a form with inputs and a submit button
+ *     should allow to submit the form
+ *   when the user fills the form incorrectly
+ *     should disabled the submit button if the form is invalid
+ *     should mark the username as invalid if it is empty
+ *     should mark the username as invalid after clear it
+ *     should not show an error for user before interaction
+ *     should show an error for user name while invalid
+ *     should mark the username as valid if it is not empty
+ *     should mark the email as invalid if it is not an email
+ *  when the user resets the form
+ *     should clear the form when the reset button is clicked
+ */
+
 describe("The sign-up form", () => {
   beforeEach(() => {
     cy.visit("http://localhost:4200/auth/sign-up");
