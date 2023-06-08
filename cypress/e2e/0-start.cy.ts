5979ea438e997cb99b0fe6be364fd4f993c164b5
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-08 13:33:59 +0200

    test: o start

--- cypress/e2e/0-start.cy.ts
+++ cypress/e2e/0-start.cy.ts
@@ -4,7 +4,36 @@
  *   should have a header
  *   should have a header with 'Activity Bookings' text
  *   should contain '©' in the footer
- *   should have a link to albertobasalo.dev
+ *   should have a link to 'https://albertobasalo.dev'
  *   should have an underline element with 'Course sample' content
  *   should have a link with css class 'secondary'
  */
+describe("The Activity Bookings home page", () => {
+  it("should be visitable", () => {
+    cy.visit("http://localhost:4200");
+  });
+  it("should have a header", () => {
+    cy.visit("http://localhost:4200");
+    cy.get("header");
+  });
+  it("should have a header with 'Activity Bookings' text", () => {
+    cy.visit("http://localhost:4200");
+    cy.get("header").contains("Activity Bookings");
+  });
+  it("should contain '©' in the footer", () => {
+    cy.visit("http://localhost:4200");
+    cy.get("footer").contains("©");
+  });
+  it("should have a link to 'https://albertobasalo.dev'", () => {
+    cy.visit("http://localhost:4200");
+    cy.get('[href="https://albertobasalo.dev"]').should("exist");
+  });
+  it("should have an underline element with 'Course sample' content", () => {
+    cy.visit("http://localhost:4200");
+    cy.get("u").should("contain.text", "Course sample");
+  });
+  it("should have a link with css class 'secondary'", () => {
+    cy.visit("http://localhost:4200");
+    cy.get("a.secondary").should("exist");
+  });
+});
