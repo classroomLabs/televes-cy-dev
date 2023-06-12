2b64c97e6db7bd482c77de350ea03d7afd7e535b
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-12 18:39:52 +0200

    feat: 3

--- cypress/e2e/3-sign_up-form.cy.ts
+++ cypress/e2e/3-sign_up-form.cy.ts
@@ -16,17 +16,69 @@
  *     should clear the form when the reset button is clicked
  */
 describe("The sign-up form", () => {
-  beforeEach(() => {});
-  it("should have a form with 4 inputs and a submit button disabled", () => {});
-  context("when the users fills the form correctly", () => {
-    beforeEach(() => {});
-    it("should allow to submit the form", () => {});
-    it("should mark all inputs as valid", () => {});
+  beforeEach(() => {
+    cy.visit("http://localhost:4200/auth/sign-up");
+  });
+  it("should have a form with 4 inputs and a submit button disabled", () => {
+    cy.get("form input").should("have.length", 4);
+    cy.get("form button[type='submit']").should("be.disabled");
+  });
+  context.only("when the users fills the form correctly", () => {
+    beforeEach(() => {
+      cy.get("#username").clear().type("John Smith");
+      cy.get("[type='email']").clear().type("JohnSmith@acme.com");
+      cy.get("[type='password']").first().clear().type("1234a");
+      cy.get("form [name='repeatPassword']").clear().type("1234a");
+    });
+    it("should allow to submit the form", () => {
+      cy.get("form button[type='submit']").should("be.enabled");
+    });
+    it("should mark all inputs as valid", () => {
+      cy.get("form input[aria-invalid='false']").should("have.length", 4);
+    });
   });
   context("when the user fills the form incorrectly", () => {
-    it("should disabled the submit button if the form is invalid", () => {});
+    it("should disabled the submit button if the form is invalid", () => {
+      cy.get("form button").should("be.disabled");
+    });
+    it("should mark the username as invalid if it is empty", () => {
+      cy.get("#username").should("have.class", "ng-invalid");
+    });
+    it("should mark the username as invalid after clear it", () => {
+      cy.get("#username").type("John");
+      cy.get("#username").clear();
+      cy.get("#username").should("have.class", "ng-invalid");
+    });
+    it("should not show an error for user before interaction", () => {
+      cy.get("[data-test='username.error']").should("not.be.visible");
+    });
+    it("should show an error for user name while invalid", () => {
+      cy.get("#username").clear().type("a");
+      cy.get("[data-test='username.error']").should("be.visible");
+    });
+    it("should mark the username as valid if it is not empty", () => {
+      cy.get("#username").clear().type("John");
+      cy.get("#username").should("have.class", "ng-valid");
+    });
+    it("should mark the email as invalid if it is not an email", () => {
+      cy.get('[type="email"]').clear().type("not-an-email");
+      cy.get('[type="email"]').should("have.class", "ng-invalid");
+    });
   });
   context("when the user resets the form", () => {
-    it("", () => {});
+    it("should clear the form when the reset button is clicked", () => {
+      // Arrange
+      cy.get("#username").clear().type("John Smith");
+      cy.get("[type='email']").clear().type("JohnSmith@acme.com");
+      cy.get("[type='password']").first().clear().type("1234a");
+      cy.get("form [name='repeatPassword']").clear().type("1234a");
+      // Act
+      cy.contains("button", "Reset").click();
+      // Assert
+      cy.get("#username").should("have.value", "");
+      cy.get('[type="email"]').should("have.value", "");
+      cy.get('[type="password"]').first().should("have.value", "");
+      cy.get('[name="repeatPassword"]').should("have.value", "");
+    });
   });
 });
