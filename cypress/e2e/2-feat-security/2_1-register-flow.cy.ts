f8cf485df3905fc19cbabff53393e5193448c3e0
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-16 12:30:39 +0200

    feat: request

--- cypress/e2e/2-feat-security/2_1-register-flow.cy.ts
+++ cypress/e2e/2-feat-security/2_1-register-flow.cy.ts
@@ -1,3 +1,5 @@
+// import { NEW_USER } from "../../support/utils";
+
 /**
  * Given a user at registration flow
  *  when sends valid new credentials
@@ -9,3 +11,28 @@
  *    should show an error dialog
  *    should display anonymous menu
  */
+describe("Given a user at registration flow", () => {
+  const PAGE_URL = "/auth/sign-up";
+  const API_URL = `${Cypress.env("apiUrl")}/register`;
+  let NEW_USER: any = null;
+  before(() => {
+    cy.fixture("new-user").then((content) => (NEW_USER = content));
+  });
+  context("when sends valid new credentials", () => {
+    beforeEach(() => {
+      cy.intercept("POST", API_URL, {
+        statusCode: 201,
+      }).as("postRegister");
+      cy.visit(PAGE_URL);
+      cy.get("#username").clear().type(NEW_USER.username);
+      cy.get('[type="email"]').clear().type(NEW_USER.email);
+      cy.get('[type="password"]').first().type(NEW_USER.password);
+      cy.get('[name="repeatPassword"]').type(NEW_USER.password);
+      cy.get("form button[type=submit]").click();
+    });
+    it("should send the form data to the server", () => {
+      cy.wait("@postRegister");
+      cy.get("@postRegister").its("request.body").should("deep.equal", NEW_USER);
+    });
+  });
+});
