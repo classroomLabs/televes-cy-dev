6b8d03245d2a152b8f45373abe7842210ec5169e
Alberto Basalo <albertobasalo@hotmail.com>
2023-06-16 12:56:12 +0200

    feat: token

--- cypress/e2e/2-feat-security/2_1-register-flow.cy.ts
+++ cypress/e2e/2-feat-security/2_1-register-flow.cy.ts
@@ -15,13 +15,16 @@ describe("Given a user at registration flow", () => {
   const PAGE_URL = "/auth/sign-up";
   const API_URL = `${Cypress.env("apiUrl")}/register`;
   let NEW_USER: any = null;
+  let TOKEN: any = null;
   before(() => {
     cy.fixture("new-user").then((content) => (NEW_USER = content));
+    cy.fixture("token").then((content) => (TOKEN = content));
   });
   context("when sends valid new credentials", () => {
     beforeEach(() => {
       cy.intercept("POST", API_URL, {
         statusCode: 201,
+        body: TOKEN,
       }).as("postRegister");
       cy.visit(PAGE_URL);
       cy.get("#username").clear().type(NEW_USER.username);
@@ -33,6 +36,18 @@ describe("Given a user at registration flow", () => {
     it("should send the form data to the server", () => {
       cy.wait("@postRegister");
       cy.get("@postRegister").its("request.body").should("deep.equal", NEW_USER);
+    });
+    it("should store the token in the local storage", () => {
+      const storedToken = localStorage.getItem("user-access-token") || "";
+      const actual = JSON.parse(storedToken);
+      const expected = TOKEN;
+      expect(actual).to.deep.equal(expected);
+    });
+    it("should redirect the user to the home page", () => {
+      cy.url().should("equal", Cypress.config("baseUrl") + "/");
+    });
+    it("should display user menu", () => {
+      cy.get("#user-menu").should("be.visible");
     });
   });
 });
